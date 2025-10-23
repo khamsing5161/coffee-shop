36feb1f081e56b8d4e8a1acc93a5bfc0514e1518
khamsing5161 <thasing436@gmail.com>
2025-10-23 17:37:56 +0700

    me

---
+++ client/src/layout/Mainlayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../components/Nav/Nav'
+
+function Mainlayout() {
+  return (
+    <section>
+        <Nav />
+        
+    </section>
+  )
+}
+
+export default Mainlayout
