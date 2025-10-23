d27dd537008e70d2711dded648d38f1bb3baafc5
khamsing5161 <thasing436@gmail.com>
2025-10-23 17:39:32 +0700

    me
    app back ent

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const PORT = process.env.PORT || 5000
+const dotenv = require('dotenv')
+const mysql = require('mysql2')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const axios =require('axios')
+
+
+dotenv.config()
+const app = express()
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+})
+
+db.connect((err) => {
+    if(err) {
+        console.log('database connection error' + err)
+    } else {
+        console.log('database connected running')
+    }
+})
+
+app.get('/', (req,res) => {
+    res.send('hello')
+})
+
+
+app.listen(PORT, () => {
+    console.log('server running on port' + PORT )
+})
